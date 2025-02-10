acb17b1f2957832c0ee38bbca1932b4147adee97
manto0530 <brainmercy@gmail.com>
2025-02-11 01:07:50 +0530

    string_cal

---
+++ app/services/string_calculator.rb
@@ -0,0 +1,20 @@
+class StringCalculator
+  def add(numbers)
+    return 0 if numbers.empty?
+
+    delimiter = ","
+    if numbers.start_with?("//")
+      parts = numbers.split("\n", 2)
+      delimiter = parts[0][2..]
+      numbers = parts[1]
+    end
+
+    numbers = numbers.gsub("\n", delimiter)
+    number_list = numbers.split(delimiter).map(&:to_i)
+
+    negatives = number_list.select { |n| n < 0 }
+    raise "negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
+
+    number_list.sum
+  end
+end
